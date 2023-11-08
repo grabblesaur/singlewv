32bb5e4c6d3ccb4be65d8e0141c56835df0dd4da
bbogdanov <bogdanov.bayar@gmail.com>
2023-11-08 14:14:22 +0300

    set webviewclient

--- app/src/main/java/com/example/singlewv/MainActivity.kt
+++ app/src/main/java/com/example/singlewv/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.example.singlewv
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
 	lateinit var webView: WebView
 
+	@SuppressLint("SetJavaScriptEnabled")
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
 		webView = findViewById(R.id.webView)
+		webView.webViewClient = WebViewClient()
+		webView.settings.javaScriptEnabled = true
+
 		webView.loadUrl("https://ya.ru/")
 	}
 }
